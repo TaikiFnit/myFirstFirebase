0fb9e03bded6d8d58684d28d8bb899b1afc0ccf5
TaikiFnit <taikifnitspg@gmail.com>
2017-01-24 13:25:39 +0900

    Initial Commit

---
+++ myFirstFireBase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFirstFireBase
+//
+//  Created by TaikiFnit on 2017/01/24.
+//  Copyright © 2017年 TaikiFnit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
