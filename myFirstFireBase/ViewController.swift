d29c50bf206b09d05e0d9367c4b573b30d90dca8
TaikiFnit <taikifnitspg@gmail.com>
2017-02-07 10:53:11 +0900

    implement realtime database on public

--- myFirstFireBase/ViewController.swift
+++ myFirstFireBase/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    
+    var ref: FIRDatabaseReference?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.ref = FIRDatabase.database().reference()
+        
+        textView.delegate = self
+        textField.delegate = self
+        
+        textLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textView.text = textView.text + textField.text! + "\n"
+        textField.text = ""
+        
+        textField.resignFirstResponder()
+        
+        save()
+        
+        return true
+    }
 
+    func save() {
+        self.ref!.child("text").setValue(["str": self.textView.text])
+    }
+    
+    func textLoad() {
+        self.ref!.child("text").observe(.value, with: {(snapShots) in
+            if snapShots.children.allObjects is [FIRDataSnapshot] {
+                print("snapShots")
+                print(snapShots.childSnapshot(forPath: "str"))
+                self.textView.text = "\(snapShots.childSnapshot(forPath: "str").value!)"
+            }
+        })
 
+    }
+    
+    @IBAction func send(_ sender: UIButton) {
+        textView.text = textView.text + textField.text! + "\n"
+        textField.text = ""
+        save()
+    }
+
+    @IBAction func clear(_ sender: UIButton) {
+        textView.text = ""
+        save()
+    }
 }
 
